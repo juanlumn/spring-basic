ea7a969440e085f2abbf1c1aa9b893f0febd0d39
juanlumn <juanlumn@gmail.com>
2023-07-11 11:40:17 +0200

    added example of usage

--- src/main/java/com/juanlumn/spring/basic/BasicApplication.java
+++ src/main/java/com/juanlumn/spring/basic/BasicApplication.java
@@ -1,13 +1,19 @@
 package com.juanlumn.spring.basic;
 
+import com.juanlumn.spring.basic.service.binarysearch.BinarySearchServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
+		// We need to get the Bean from the Application Context
+		ApplicationContext applicationContext = SpringApplication.run(BasicApplication.class, args);
+		BinarySearchServiceImpl bean = applicationContext.getBean(BinarySearchServiceImpl.class);
+
+		System.out.println(bean.binarySearch(new int[]{2,5,1,6,123}, 5));
 	}
 
 }
