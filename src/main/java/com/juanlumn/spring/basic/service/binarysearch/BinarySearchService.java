1ca560cb4fbe493bdd869d587dce26e7d1759e62
juanlumn <juanlumn@gmail.com>
2023-07-11 11:40:02 +0200

    added BinarySearchService and implementation

---
+++ src/main/java/com/juanlumn/spring/basic/service/binarysearch/BinarySearchService.java
@@ -0,0 +1,5 @@
+package com.juanlumn.spring.basic.service.binarysearch;
+
+public interface BinarySearchService {
+    int binarySearch(int[] numbers, int number);
+}
---
+++ src/main/java/com/juanlumn/spring/basic/service/binarysearch/BinarySearchServiceImpl.java
@@ -0,0 +1,25 @@
+package com.juanlumn.spring.basic.service.binarysearch;
+
+import com.juanlumn.spring.basic.service.sort.SortAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class BinarySearchServiceImpl implements BinarySearchService {
+
+    @Autowired
+    private final SortAlgorithm sortAlgorithm;
+
+    public BinarySearchServiceImpl(SortAlgorithm sortAlgorithm) {
+        this.sortAlgorithm = sortAlgorithm;
+    }
+
+    @Override
+    public int binarySearch(int[] numbers, int number) {
+        // Please note this code is for Spring example purposes
+        sortAlgorithm.sort(numbers);
+        return Arrays.binarySearch(numbers, number);
+    }
+}
