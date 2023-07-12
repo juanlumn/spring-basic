deeead9877e137a83c386dde1529015faa198681
juanlumn <juanlumn@gmail.com>
2023-07-12 13:26:38 +0200

    added lifecycle methods

--- src/main/java/com/juanlumn/spring/basic/service/binarysearch/BinarySearchServiceImpl.java
+++ src/main/java/com/juanlumn/spring/basic/service/binarysearch/BinarySearchServiceImpl.java
@@ -1,6 +1,10 @@
 package com.juanlumn.spring.basic.service.binarysearch;
 
 import com.juanlumn.spring.basic.service.sort.SortAlgorithm;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,12 +14,24 @@ import java.util.Arrays;
 @Component
 public class BinarySearchServiceImpl implements BinarySearchService {
 
+    private final Logger LOG = LoggerFactory.getLogger(BinarySearchServiceImpl.class);
+
     @Autowired
     @Qualifier("quick")
     private final SortAlgorithm sortAlgorithm;
 
     public BinarySearchServiceImpl(SortAlgorithm sortAlgorithm) {
         this.sortAlgorithm = sortAlgorithm;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.info("This is happening just after the Bean has been added to the context");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        LOG.info("This is happening just before the Bean has been removed from the context");
     }
 
     @Override
